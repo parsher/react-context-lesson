c7bfc6b74e2e6922b240a0fa6c6bba85ee100647
parsher <26663229+parsher@users.noreply.github.com>
2020-03-03 16:39:38 +0900

    remove redux

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { useContext } from "react";
+import CollectionContext from "../../contexts/collections/collections.context";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import CollectionPreview from '../collection-preview/collection-preview.component';
+import "./collections-overview.styles.scss";
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+const CollectionsOverview = () => {
+  const collectionsMap = useContext(CollectionContext);
+  const collections = Object.keys(collectionsMap).map(
+    key => collectionsMap[key]
+  );
 
-import './collections-overview.styles.scss';
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
-  </div>
-);
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
